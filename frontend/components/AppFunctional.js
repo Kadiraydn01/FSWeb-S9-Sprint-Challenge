2f930ac2175e677cdcee080e158591ec83f6333d
Kadiraydn01 <kadiraydn01@gmail.com>
2023-08-27 00:57:04 +0300

    Testler bitti

--- frontend/components/AppFunctional.js
+++ frontend/components/AppFunctional.js
@@ -1,107 +1,130 @@
+import axios from "axios";
 import React, { useState } from "react";
 
-export default function AppFunctional(props) {
-  const initialMessage = "";
-  const initialEmail = "";
-  const initialSteps = 0;
-  const initialIndex = 4;
+const initialMessage = "";
+const initialEmail = "";
+const initialSteps = 0;
+const initialIndex = 4;
 
-  const [message, setMessage] = useState(initialMessage);
+export default function AppFunctional(props) {
+  const [deger, setDeger] = useState(initialIndex);
+  const [sayac, setSayac] = useState(initialSteps);
   const [email, setEmail] = useState(initialEmail);
-  const [steps, setSteps] = useState(initialSteps);
-  const [index, setIndex] = useState(initialIndex);
+  const [message, setMessage] = useState(initialMessage);
+
+  const postObject = {
+    email: email,
+    x: (deger + 1) % 3 == 0 ? 3 : (deger + 1) % 3,
+    y: deger < 3 ? 1 : deger < 6 ? 2 : deger < 9 ? 3 : false,
+    steps: sayac,
+  };
 
   function getXY() {
-    const gridSize = 3;
-    const xCoordinate = (index % gridSize) + 1;
-    const yCoordinate = Math.floor(index / gridSize) + 1;
-    return { x: xCoordinate, y: yCoordinate };
+    const xyArray = [];
+    for (let y = 1; y < 4; y++) {
+      for (let x = 1; x < 4; x++) {
+        xyArray.push(`(${x},${y})`);
+      }
+    }
+    return xyArray[deger];
   }
 
-  function getXYMesaj() {
-    const coordinates = getXY();
-    return `Koordinatlar (${coordinates.x}, ${coordinates.y})`;
-  }
+  function getXYMesaj() {}
 
   function reset() {
-    setSteps(initialSteps);
-    setIndex(initialIndex);
-    setMessage(initialMessage);
-    setEmail(initialEmail);
+    setSayac(0);
+    setDeger(initialIndex);
+    setMessage("");
+    setEmail("");
   }
 
   function sonrakiIndex(yon) {
-    const gridSize = 3;
-    const currentIndex = index;
-    let nextIndex = currentIndex;
-
-    if (yon === "left" && currentIndex % gridSize !== 0) {
-      nextIndex = currentIndex - 1;
-    } else if (yon === "up" && currentIndex >= gridSize) {
-      nextIndex = currentIndex - gridSize;
-    } else if (yon === "right" && currentIndex % gridSize !== gridSize - 1) {
-      nextIndex = currentIndex + 1;
-    } else if (yon === "down" && currentIndex < gridSize * gridSize - 3) {
-      nextIndex = currentIndex + gridSize;
+    if (
+      yon === "left" &&
+      (deger == 1 ||
+        deger == 2 ||
+        deger == 4 ||
+        deger == 5 ||
+        deger == 7 ||
+        deger == 8)
+    ) {
+      setDeger(deger - 1);
+      setSayac(sayac + 1);
+      setMessage("");
+    } else if (yon === "left") {
+      setMessage("Sola gidemezsiniz");
     }
 
-    return nextIndex;
-  }
+    if (
+      yon === "right" &&
+      (deger == 0 ||
+        deger == 1 ||
+        deger == 3 ||
+        deger == 4 ||
+        deger == 6 ||
+        deger == 7)
+    ) {
+      setDeger(deger + 1);
+      setSayac(sayac + 1);
+      setMessage("");
+    } else if (yon === "right") {
+      setMessage("Sağa gidemezsiniz");
+    }
 
-  function ilerle(evt) {
-    const direction = evt.target.id;
-    const nextIndex = sonrakiIndex(direction);
-    setIndex(nextIndex);
-    setSteps(steps + 1);
+    if (
+      yon === "up" &&
+      (deger == 3 ||
+        deger == 4 ||
+        deger == 5 ||
+        deger == 6 ||
+        deger == 7 ||
+        deger == 8)
+    ) {
+      setDeger(deger - 3);
+      setSayac(sayac + 1);
+      setMessage("");
+    } else if (yon === "up") {
+      setMessage("Yukarıya gidemezsiniz");
+    }
+
+    if (yon === "down" && deger < 6) {
+      setDeger(deger + 3);
+      setSayac(sayac + 1);
+      setMessage("");
+    } else if (yon === "down") {
+      setMessage("Aşağıya gidemezsiniz");
+    }
   }
 
   function onChange(evt) {
-    const newEmail = evt.target.value;
-    setEmail(newEmail);
+    setEmail(evt.target.value);
   }
 
   function onSubmit(evt) {
     evt.preventDefault();
-
-    const coordinates = getXY();
-    const payload = {
-      x: coordinates.x,
-      y: coordinates.y,
-      steps: steps,
-      email: email,
-    };
-
-    fetch("http://localhost:9000/api/result", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error("API isteği başarısız");
-        }
-        return response.json();
+    axios
+      .post("http://localhost:9000/api/result", postObject)
+      .then(function (response) {
+        console.log(response);
+        setMessage(response.data.message);
+        setEmail("");
       })
-      .then((data) => {
-        setMessage(data.message);
-      })
-      .catch((error) => {
-        console.error("Hata:", error);
+      .catch(function (error) {
+        console.log(error.response.data.message);
+        setMessage(error.response.data.message);
       });
   }
 
   return (
     <div id="wrapper" className={props.className}>
       <div className="info">
-        <h3 id="coordinates">{getXYMesaj()}</h3>
-        <h3 id="steps">{steps} kere ilerlediniz</h3>
+        <h3 id="coordinates">Koordinatlar {getXY()}</h3>
+        <h3 id="steps">{sayac} kere ilerlediniz</h3>
       </div>
       <div id="grid">
         {[0, 1, 2, 3, 4, 5, 6, 7, 8].map((idx) => (
-          <div key={idx} className={`square${idx === index ? " active" : ""}`}>
-            {idx === index ? "B" : null}
+          <div key={idx} className={`square${idx === deger ? " active" : ""}`}>
+            {idx === deger ? "B" : null}
           </div>
         ))}
       </div>
@@ -109,31 +132,31 @@ export default function AppFunctional(props) {
         <h3 id="message">{message}</h3>
       </div>
       <div id="keypad">
-        <button id="left" onClick={ilerle}>
+        <button onClick={() => sonrakiIndex("left")} id="left">
           SOL
         </button>
-        <button id="up" onClick={ilerle}>
+        <button onClick={() => sonrakiIndex("up")} id="up" data-testid="yukarı">
           YUKARI
         </button>
-        <button id="right" onClick={ilerle}>
+        <button onClick={() => sonrakiIndex("right")} id="right">
           SAĞ
         </button>
-        <button id="down" onClick={ilerle}>
+        <button onClick={() => sonrakiIndex("down")} id="down">
           AŞAĞI
         </button>
-        <button id="reset" onClick={reset}>
+        <button onClick={reset} id="reset">
           reset
         </button>
       </div>
-      <form onSubmit={onSubmit}>
+      <form>
         <input
+          onChange={onChange}
+          value={email}
           id="email"
           type="email"
           placeholder="email girin"
-          value={email}
-          onChange={onChange}
         ></input>
-        <input id="submit" type="submit"></input>
+        <input onClick={onSubmit} id="submit" type="submit"></input>
       </form>
     </div>
   );
